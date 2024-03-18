ce001741bcc02d5b4e09323f85f073124c4caba5
Jacobtread <jacobtread@gmail.com>
2024-03-18 20:31:36 +1300

    refactor: switch to obsidian-plugin-cli
    Switched build tooling to obsidian-plugin-cli

--- postBuild.mjs
+++
@@ -1,6 +0,0 @@
-// Script for copying files to the build directory after build
-
-import files from "fs/promises";
-
-await files.copyFile("src/styles.css", "build/styles.css");
-await files.copyFile("manifest.json", "build/manifest.json");
